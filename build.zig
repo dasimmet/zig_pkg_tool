1dc00d42f9015914c274b218f1e6b8b94f6d3207
Tobias Simetsreiter <dasimmet@gmail.com>
2024-11-14 14:43:47 +0100

    add vaxis example

--- build.zig
+++ build.zig
@@ -1,5 +1,23 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b;
+    const target = b.standardTargetOptions(.{});
+    const opt = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "menuconfig",
+        .root_source_file = b.path("src/menuconfig.zig"),
+        .target = target,
+        .optimize = opt,
+    });
+    const run = b.addRunArtifact(exe);
+    b.installArtifact(exe);
+
+    const dep = b.dependency("vaxis", .{
+        .target = target,
+        .optimize = opt,
+    });
+    exe.root_module.addImport("vaxis", dep.module("vaxis"));
+
+    const config_step = b.step("menuconfig", "example menu");
+    config_step.dependOn(&run.step);
 }
