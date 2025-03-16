28f21c66eb4c9ca75b4e673789cfe0ca92e6670f
Tobias Simetsreiter <dasimmet@gmail.com>
2025-03-16 13:58:51 +0100

    add injection point to build runner

--- src/zig_build_runner.zig
+++ src/zig_build_runner.zig
@@ -24,6 +24,14 @@ pub const std_options: std.Options = .{
 };
 
 pub fn main() !void {
+    return mainBuild(null);
+}
+
+pub const buildExecArgs = struct {
+    executeBuildFn: fn (*std.Build, ?*anyopaque) anyerror!void,
+    ctx: ?*anyopaque = null,
+};
+pub fn mainBuild(execArgs: ?buildExecArgs) !void {
     // Here we use an ArenaAllocator backed by a page allocator because a build is a short-lived,
     // one shot program. We don't need to waste time freeing memory and finding places to squish
     // bytes into. So we free everything all at once at the very end.
@@ -365,6 +373,10 @@ pub fn main() !void {
     }
 
     validateSystemLibraryOptions(builder);
+
+    if (execArgs) |exec| {
+        return exec.executeBuildFn(builder, exec.ctx);
+    }
 
     const stdout_writer = io.getStdOut().writer();
 
