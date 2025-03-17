2bc968211c69949359fd6305c6e77774bf519898
Tobias Simetsreiter <dasimmet@gmail.com>
2025-03-17 01:01:11 +0100

    refactore dot runner

--- src/runner-dot.zig
+++ src/runner-dot.zig
@@ -5,7 +5,7 @@ pub const dependencies = @import("@dependencies");
 pub fn main() !void {
     return try @import("runner-zig.zig").mainBuild(.{
         .executeBuildFn = build_main,
-        .ctx = undefined,
+        .ctx = null,
     });
 }
 
@@ -13,71 +13,66 @@ pub fn build_main(b: *std.Build, targets: []const []const u8, ctx: ?*anyopaque) 
     _ = ctx;
     const stdout = std.io.getStdOut().writer();
     try stdout.writeAll("digraph {\n");
+    var dotfile: DotFileWriter = .{};
     if (targets.len == 0) {
-        try iterate_steps(b, &b.default_step.*, 0, &printStep);
+        try dotfile.writeSteps(b, &b.default_step.*, stdout);
     } else {
         for (targets) |target| {
             if (b.top_level_steps.get(target)) |step| {
-                try iterate_steps(b, &step.step, 0, &printStep);
+                try dotfile.writeSteps(b, &step.step, stdout);
             }
         }
     }
     try stdout.writeAll("}\n");
 }
 
-pub const DepFn = fn (*std.Build, *std.Build.Step, usize, u32) anyerror!void;
-
-var iterator: u32 = 0;
-var dotMap: std.AutoHashMapUnmanaged(usize, struct {
-    id: u32,
-    visited: bool,
-}) = .empty;
-pub fn iterate_steps(b: *std.Build, step: *std.Build.Step, depth: usize, depfn: *const DepFn) !void {
-    const dot_entry = try dotMap.getOrPut(b.allocator, @intFromPtr(step));
-    if (dot_entry.found_existing) {
-        if (dot_entry.value_ptr.visited) return;
-        dot_entry.value_ptr.visited = true;
-    } else {
-        dot_entry.value_ptr.* = .{
-            .id = iterator,
-            .visited = true,
-        };
-        iterator += 1;
-    }
-
-    try depfn(b, step, depth, dot_entry.value_ptr.*.id);
-
-    for (step.dependencies.items) |dep_step| {
-        try iterate_steps(b, dep_step, depth + 1, depfn);
-    }
-}
-
-pub fn printStep(b: *std.Build, step: *std.Build.Step, depth: usize, i: u32) !void {
-    _ = depth;
-    const stdout = std.io.getStdOut().writer();
-
-    try stdout.print("\"N{d}\" [label=\"{s}\"]\n", .{ i, step.name });
-    for (step.dependencies.items) |dep_step| {
-        const dot_entry = try dotMap.getOrPut(b.allocator, @intFromPtr(dep_step));
-        if (!dot_entry.found_existing) {
-            dot_entry.value_ptr.* = .{
-                .id = iterator,
-                .visited = false,
+pub const DotFileWriter = struct {
+    id: u32 = 0,
+    depth: u32 = 0,
+    steps: std.AutoHashMapUnmanaged(usize, struct {
+        id: u32 = 0,
+        visited: bool = false,
+    }) = .empty,
+    pub fn writeSteps(self: *@This(), b: *std.Build, step: *std.Build.Step, writer: anytype) !void {
+        const step_entry = try self.steps.getOrPut(b.allocator, @intFromPtr(step));
+        if (step_entry.found_existing) {
+            if (step_entry.value_ptr.visited) return;
+            step_entry.value_ptr.visited = true;
+        } else {
+            step_entry.value_ptr.* = .{
+                .id = self.id,
+                .visited = true,
             };
-            iterator += 1;
+            self.id += 1;
         }
-        try stdout.print("\"N{d}\" -> \"N{d}\"\n", .{
-            i,
-            dot_entry.value_ptr.id,
-        });
+
+        try self.printStep(b, step, step_entry.value_ptr.*.id, writer);
+
+        self.depth += 1;
+        for (step.dependencies.items) |dep_step| {
+            try self.writeSteps(b, dep_step, writer);
+        }
+        self.depth -= 1;
     }
-    // return printStructStdout(.{
-    //     .i = i,
-    //     .depth = depth,
-    //     .name = step.name,
-    //     .build_root = step.owner.build_root.path.?,
-    // });
-}
+
+    pub fn printStep(self: *@This(), b: *std.Build, step: *std.Build.Step, i: u32, writer: anytype) !void {
+        try writer.print("\"N{d}\" [label=\"{s}\"]\n", .{ i, step.name });
+        for (step.dependencies.items) |dep_step| {
+            const dot_entry = try self.steps.getOrPut(b.allocator, @intFromPtr(dep_step));
+            if (!dot_entry.found_existing) {
+                dot_entry.value_ptr.* = .{
+                    .id = self.id,
+                    .visited = false,
+                };
+                self.id += 1;
+            }
+            try writer.print("\"N{d}\" -> \"N{d}\"\n", .{
+                i,
+                dot_entry.value_ptr.id,
+            });
+        }
+    }
+};
 
 pub fn printStructStdout(item: anytype) !void {
     const stdout = std.io.getStdOut().writer();
