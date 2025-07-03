4fb97892e7be048a127acb952d7ee9f00a8373fc
Tobias Simetsreiter <dasimmet@gmail.com>
2025-07-03 18:11:20 +0200

    also upgrade version string

--- src/runner-zig.zig
+++ src/runner-zig.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 // master is on this state:
-// https://github.com/ziglang/zig/commit/b5a5260546ddd8953e493f75c5ee12c5a853263b
-// 0.15.0-dev.168+b636d56d6
+// https://github.com/ziglang/zig/commit/31bc6d5a9ddaf09511d8e5dc6017957adec0564b
+// 0.15.0-dev.911+31bc6d5a9
 pub const runner = if (builtin.zig_version.order(std.SemanticVersion.parse("0.14.0") catch unreachable) == .gt)
     @import("runner-zig-master.zig")
 else
