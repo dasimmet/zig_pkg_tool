3cd4bc787e5798c226e37713e6a18096fafd2485
Tobias Simetsreiter <dasimmet@gmail.com>
2025-11-16 21:06:52 +0100

    add version to build runner file

--- src/runner-zig.zig
+++ src/runner-zig.zig
@@ -4,6 +4,7 @@ const builtin = @import("builtin");
 const later_than_zig_15 = builtin.zig_version.order(std.SemanticVersion.parse("0.15.99") catch unreachable) == .gt;
 
 pub const runner = if (later_than_zig_15)
+    // 0.16.0-dev.1354+94e98bfe8
     @import("runner-zig-master.zig")
 else
     @import("runner-zig-0.15.X.zig");
