c8c8bd3c62a457a8ddd012d01f3de6272ba1b40f
Tobias Simetsreiter <dasimmet@gmail.com>
2025-12-02 21:37:24 +0100

    wip

--- src/buildrunner/zonparse.zig
+++ src/buildrunner/zonparse.zig
@@ -36,28 +36,38 @@ pub const align_one = if (later_than_zig_14)
 else
     1;
 
-pub fn cwdReadFileAllocZ(
+pub const cwdReadFileAllocZ = if (!later_than_zig_15)
+    cwdReadFileAllocZ_14
+else
+    cwdReadFileAllocZ_master;
+
+pub fn cwdReadFileAllocZ_14(
     subpath: []const u8,
     allocator: std.mem.Allocator,
     max_bytes: usize,
 ) ![:0]const u8 {
     const cwd = std.fs.cwd();
-    if (comptime later_than_zig_14) {
-        return cwd.readFileAllocOptions(
-            allocator,
-            subpath,
-            max_bytes,
-            null,
-            align_one,
-            0,
-        );
-    } else {
-        return cwd.readFileAllocOptions(
-            subpath,
-            allocator,
-            std.Io.Limit.limited(max_bytes),
-            align_one,
-            0,
-        );
-    }
+    return cwd.readFileAllocOptions(
+        allocator,
+        subpath,
+        max_bytes,
+        null,
+        align_one,
+        0,
+    );
+}
+
+pub fn cwdReadFileAllocZ_master(
+    subpath: []const u8,
+    allocator: std.mem.Allocator,
+    max_bytes: usize,
+) ![:0]const u8 {
+    const cwd = std.fs.cwd();
+    return cwd.readFileAllocOptions(
+        subpath,
+        allocator,
+        std.Io.Limit.limited(max_bytes),
+        align_one,
+        0,
+    );
 }
